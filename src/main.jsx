25933cebc50affbac770147044a223bb7914b429
Pankaj Pant <pankajpant@Pankajs-MacBook-Pro.local>
2025-01-07 20:57:23 -0500

    add: route

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/ideals-website/" element={<App />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
