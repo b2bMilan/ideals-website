f8c25992689f7d452cc7814e6cee0bb046e944d7
Pankaj Pant <pankajpant@Pankajs-MacBook-Pro.local>
2025-01-12 14:28:09 -0500

    add: dashboard

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,14 +3,36 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "#f8f8f8" }}>
-      <Link to="/ideals-website/" style={{ marginRight: "20px" }}>
-        Home
-      </Link>
-      <Link to="/ideals-website/guide" style={{ marginRight: "20px" }}>Instructions</Link>
-      <Link to="/ideals-website/contact" style={{ marginRight: "20px" }}>Contact</Link>
-      <Link to="/ideals-website/privacy" style={{ marginRight: "10px" }}>Privacy</Link>
-    </nav>
+    <nav style={{ 
+        padding: "10px", 
+        backgroundColor: "#f8f8f8", 
+        display: "flex", 
+        alignItems: "center", 
+        justifyContent: "space-between" 
+      }}>
+        <Link to="/ideals-website/">
+          <img 
+            src="src/assets/Logo.png" 
+            alt="Logo" 
+            style={{ height: "40px", padding: "10px" }} 
+          />
+        </Link>
+        <div>
+          <Link to="/ideals-website/" style={{ marginRight: "20px" }}>
+            Home
+          </Link>
+          <Link to="/ideals-website/guide" style={{ marginRight: "20px" }}>
+            Instructions
+          </Link>
+          <Link to="/ideals-website/contact" style={{ marginRight: "20px" }}>
+            Contact
+          </Link>
+          <Link to="/ideals-website/privacy">
+            Privacy
+          </Link>
+        </div>
+      </nav>
+      
   );
 };
 
