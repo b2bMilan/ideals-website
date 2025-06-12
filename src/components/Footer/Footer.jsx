6f066de8cbe5fba3c217cc4e2cd4295492426e0f
Pankaj Pant <pankajpant@Pankajs-MacBook-Pro.local>
2025-06-12 07:24:54 -0400

    added hashrouter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
   return (
     <footer>
       <div className="links">
-        <Link to="/ideals-website/guide" style={{ marginRight: "20px" }}>Instructions</Link>
-        <Link to="/ideals-website/contact" style={{ marginRight: "20px" }}>Contact</Link>
-        <Link to="/ideals-website/privacy" style={{ marginRight: "10px" }}>Privacy</Link>
+        <Link to="/guide" style={{ marginRight: "20px" }}>Instructions</Link>
+        <Link to="/contact" style={{ marginRight: "20px" }}>Contact</Link>
+        <Link to="/privacy" style={{ marginRight: "10px" }}>Privacy</Link>
       </div>
       <div className="copyright">
         © {new Date().getFullYear()} iDeals. All rights reserved.
